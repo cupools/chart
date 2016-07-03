3255365837bc9e2f57340f7572432e1875cbc43d
cupools <cupools@gmail.com>
2016-07-03 22:05:10 +0800

    Update pie

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import pie from './type/pie';
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 pie.render(ctx);
 
 if (module.hot) {
--- src/type/pie.js
+++ src/type/pie.js
@@ -1,28 +1,41 @@
 'use strict';
 
-import { data } from '../data/sex.js';
-
 export default {
     render(ctx) {
-        let renderDate = [1, 3, 6, 9];
-        let sum = renderDate.reduce((a, b) => (a + b));
+        let renderDate = [2, 4, 6, 9];
+        let radius = 100;
+        let textRadius = 60;
+        let pos = [250, 250];
 
+        let sum = renderDate.reduce((a, b) => (a + b));
         let last = 0 - Math.PI / 2;
 
         renderDate.map((count, idx) => {
             let startAngle = last;
-            let endAngle = last = count / sum * Math.PI * 2 + startAngle;
+            let endAngle = count / sum * Math.PI * 2 + startAngle;
 
             ctx.save();
             ctx.fillStyle = '#00' + count;
             ctx.strokeStyle = '#00' + count;
             ctx.beginPath();
-            ctx.moveTo(250, 250);
-            ctx.arc(250, 250, 100, startAngle, endAngle, false);
+            ctx.moveTo(pos[0], pos[1]);
+            ctx.arc(pos[0], pos[1], radius, startAngle, endAngle, false);
             ctx.closePath();
             ctx.fill();
             ctx.stroke();
             ctx.restore();
-        })
+
+            ctx.save();
+            let angle = (endAngle - startAngle + Math.PI) / 2 + last;
+            let txt = count;
+            let x = pos[0] + Math.sin(angle) * textRadius;
+            let y = pos[1] - Math.cos(angle) * textRadius;
+            ctx.font = '14px Arial';
+            ctx.fillStyle = '#fff';
+            ctx.fillText(txt, x, y);
+            ctx.restore();
+
+            last = endAngle;
+        });
     }
 }
