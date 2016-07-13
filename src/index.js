2685b59b62ba805ad2574a55fe809a7fe35e4639
liyh <lyh106415@alibaba-inc.com>
2016-07-13 21:25:04 +0800

    update Pie

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 'use strict';
 
-import 'hidpi-canvas/dist/hidpi-canvas';
+// import 'hidpi-canvas/dist/hidpi-canvas';
 import './utils/polyfill.js';
 import Pie from './type/Pie';
 import Line from './type/Line';
 
 export { Pie, Line };
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+Pie.render(ctx, {
+    renderDate: [{
+        count: 1,
+        texture: '#f00'
+    }, {
+        count: 2,
+        texture: '#f60'
+    }, {
+        count: 3,
+        texture: '#6f0'
+    }],
+    textRadius: 110
+});
 
 if (module.hot) {
     module.hot.accept();
--- src/type/Pie.js
+++ src/type/Pie.js
@@ -4,6 +4,7 @@ import _ from '../utils/util';
 import Circle from './Circle';
 
 const PI = Math.PI;
+const outAngleMap = [PI / 4 * 7, PI / 4, PI / 4 * 3, PI / 4 * 5];
 
 const defaultOptions = {
     renderDate: [],
@@ -17,81 +18,137 @@ export default {
     render(ctx, opt = {}) {
         let options = Object.assign({}, defaultOptions, opt);
         let {renderDate, radius, textRadius, outRadius, position} = options;
-        let sum = renderDate.reduce((a, b) => (a.count + b.count));
+        let sum = renderDate.reduce((a, b) => ((a.count ? a.count : a) + b.count));
+        let len = renderDate.length;
         let last = 0 - PI / 2;
 
-        renderDate.map(item => _.assignWith(item, {
-            radius,
-            textRadius,
-            outRadius
-        }));
+        renderDate.map(item => {
+            let {count} = item;
+            let startAngle = last;
+            let endAngle = count / sum * PI * 2 + startAngle;
+            let middleAngle = (endAngle - startAngle + PI) / 2 + last;
 
-        renderDate.map(({count, outRadius, texture, textRadius} , idx) => {
+            last = endAngle;
+
+            return _.assignWith(item, {
+                radius,
+                textRadius,
+                outRadius,
+                startAngle,
+                endAngle,
+                middleAngle
+            });
+        });
+
+        // 绘制主要的扇形区域
+        renderDate.map((item, idx) => {
+            let {count, radius, texture, outRadius, startAngle, endAngle, middleAngle} = item;
+
             if (count === 0) {
                 return false;
             }
 
-            let startAngle = last;
-            let endAngle = count / sum * PI * 2 + startAngle;
-            let angle = (endAngle - startAngle + PI) / 2 + last;
-            let x,
-                y;
-
-            outRadius = sum / count !== 2 ? outRadius : 0;
+            let pos = new Circle(position, outRadius).pos(middleAngle);
+            outRadius = (len === 2 && sum / count !== 2) ? outRadius : 0;
 
             ctx.save();
+            ctx.beginPath();
             ctx.fillStyle = texture;
             ctx.strokeStyle = texture;
-            ctx.beginPath();
-            [x, y] = new Circle(position, outRadius).pos(angle);
 
-            ctx.moveTo(x, y);
-            ctx.arc(x, y, radius, startAngle, endAngle, false);
-            ctx.closePath();
+            ctx.moveTo(...pos);
+            ctx.arc(...pos, radius, startAngle, endAngle, false);
             ctx.fill();
             ctx.stroke();
-
+            ctx.closePath();
             ctx.restore();
+        });
 
-            // TODO, static angle
-            if (textRadius) {
-                let pos;
+        // 绘制注解
+        renderDate.map((item, idx) => {
+            let {count, radius, outRadius, textRadius, middleAngle} = item;
+
+            if (count === 0 || !textRadius) {
+                return false;
+            }
+
+            let txt = count + '枚';
+
+            if (len < 3) {
+                // 绘制起始点
+                let pos = new Circle(position, Math.floor((radius + outRadius) / 4 * 3)).pos(middleAngle);
+                let outAngle = outAngleMap[Math.floor(middleAngle / PI * 2)];
 
                 ctx.save();
                 ctx.fillStyle = '#333';
                 ctx.strokeStyle = '#333';
 
                 ctx.beginPath();
-                pos = new Circle(position, radius / 2 + outRadius).pos(angle);
                 ctx.moveTo(...pos);
                 ctx.arc(...pos, 2, 0, PI * 2, false);
                 ctx.fillStyle = '#333';
                 ctx.fill();
                 ctx.closePath();
 
+                // 绘制折线
                 ctx.beginPath();
                 ctx.moveTo(...pos);
-                pos = new Circle(position, textRadius + outRadius).pos(angle);
+
+                pos = new Circle(pos, radius - textRadius).pos(outAngle - PI / 2);
+
                 ctx.lineTo(...pos);
                 pos[0] = pos[0] + (idx ? -15 : 15);
                 ctx.lineTo(...pos);
                 ctx.stroke();
                 ctx.closePath();
 
-                let txt = count + '枚';
-                pos[0] = pos[0] + (idx ? -6 : 6);
+                // 绘制注解
+                ctx.beginPath();
+                pos[0] = pos[0] + (idx ? -4 : 4);
                 pos[1] = pos[1] + 5;
                 ctx.moveTo(...pos);
                 ctx.font = '10px Arial';
                 ctx.textAlign = idx ? 'right' : 'left';
+                ctx.fillStyle = '#333';
+                ctx.fillText(txt, ...pos);
+
+                ctx.restore();
+            } else {
+                let pos = new Circle(position, Math.floor((radius + outRadius) / 4 * 3)).pos(middleAngle);
+                let isRight = middleAngle > PI;
+                ctx.save();
+                ctx.fillStyle = '#333';
+                ctx.strokeStyle = '#333';
+
+                // 绘制起始点
+                ctx.beginPath();
+                ctx.moveTo(...pos);
+                ctx.arc(...pos, 2, 0, PI * 2, false);
+                ctx.fillStyle = '#333';
+                ctx.fill();
+                ctx.closePath();
+
+                // 绘制折线
+                ctx.beginPath();
+                ctx.moveTo(...pos);
+                pos = new Circle(position, textRadius + outRadius).pos(middleAngle);
+                ctx.lineTo(...pos);
+                pos[0] = pos[0] + (isRight ? -15 : 15);
+                ctx.lineTo(...pos);
+                ctx.stroke();
+                ctx.closePath();
+
+                // 绘制注解
+                pos[0] = pos[0] + (isRight ? -6 : 6);
+                pos[1] = pos[1] + 5;
+                ctx.moveTo(...pos);
+                ctx.font = '10px Arial';
+                ctx.textAlign = isRight ? 'right' : 'left';
 
                 ctx.fillStyle = '#333';
                 ctx.fillText(txt, ...pos);
                 ctx.restore();
             }
-
-            last = endAngle;
-
         });
     }
 };
