326acb298412d7917eb50eccf5c694aafbf83141
liyh <lyh106415@alibaba-inc.com>
2016-07-24 23:44:08 +0800

    update examples

--- examples/line.js
+++ examples/line.js
@@ -1,36 +1,36 @@
 /* global chart, Hammer */
-'use strict';
+'use strict'
 
-var Line = chart.Line;
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+var Line = chart.Line
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
 
-ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 // fix canvas blur
-ctx.translate(0.5, 0.5);
+ctx.translate(0.5, 0.5)
 
-let el = Line.init(ctx);
-el.render();
+let el = Line.init(ctx)
+el.render()
 
-let mc = new Hammer.Manager(canvas);
+let mc = new Hammer.Manager(canvas)
 mc.add(new Hammer.Pan({
     direction: Hammer.DIRECTION_HORIZONTAL,
     threshold: 10
-}));
+}))
 
-let flag = 0;
+let flag = 0
 mc.on('panstart', function() {
-    flag = el.ctl.offsetLeft;
-});
+    flag = el.ctl.offsetLeft
+})
 
 mc.on('panmove', function(ev) {
-    el.setOffsetLeft(flag - ev.deltaX / 1);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    el.render();
-});
+    el.setOffsetLeft(flag - ev.deltaX / 1)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    el.render()
+})
 
 mc.on('panend pancancel', function() {
-    let {offsetX} = el.ctl;
-    el.swipeTo(offsetX);
-});
+    let {offsetX} = el.ctl
+    el.swipeTo(offsetX)
+})
--- examples/pie.js
+++ examples/pie.js
@@ -1,13 +1,13 @@
 /* global chart */
-'use strict';
+'use strict'
 
-var Pie = chart.Pie;
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+var Pie = chart.Pie
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
 
-ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.clearRect(0, 0, canvas.width, canvas.height)
 Pie.render(ctx, {
-    renderDate: [{
+    renderData: [{
         count: 1,
         texture: '#f00',
         outRadius: 0
@@ -15,4 +15,4 @@ Pie.render(ctx, {
         count: 2,
         texture: '#f60'
     }]
-});
+})
